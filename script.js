273c90190c3dfba2569ac7927a2b6829e82c4c57
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-11-26 02:19:55 +0200

    Creating Navbar and setting up scrollTrigger

--- script.js
+++ script.js
@@ -12,25 +12,35 @@ var body = document.querySelector("body");
 
 
 // Event to enter mouse in video container
-function enterInVideo() {
+function enterMouse() {
     videoContainer.addEventListener("mouseenter", function () {
         play.style.transform = "scale(1)";
     })
+    document.querySelector(".products-container").addEventListener("mouseenter", function () {
+        document.querySelector("#effect").style.transform = "scale(1)";
+    })
 }
-enterInVideo();
+enterMouse();
 
 // Event to leave mouse in video container
-function leaveVideo() {
+function leaveMouse() {
     videoContainer.addEventListener("mouseleave", function () {
         play.style.transform = "scale(0)";
     })
+    document.querySelector(".products-container").addEventListener("mouseleave", function () {
+        document.querySelector("#effect").style.transform = "scale(0)";
+    })
 }
-leaveVideo();
+leaveMouse();
 
 // Event to move mouse in video container and scaling div so that it will follow mouse
 function mouseMove() {
     body.addEventListener("mousemove", function (dets) {
         playContainer.style.transform = `translate(${dets.pageX - 65}px, ${dets.pageY - 65}px)`;
+    })
+
+    body.addEventListener("mousemove", function (dets) {
+        document.querySelector("#product-hover").style.transform = `translate(${dets.pageX - 128}px, ${dets.pageY - 128}px)`;
     })
 }
 mouseMove();
